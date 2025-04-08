e0c7f2e71a07474b52f726e26b2d5b3fe216bbe0
Gabrielhenriquemelo <gabrielhenriquemelo116@gmail.com>
2025-04-08 12:23:42 -0300

    08/04/25

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,24 @@
+import BoasVindas from "./exemplo1"
+
+function App() {
+  
+
+  return (
+   <div>
+    <h1>Boas vindas ao react com JavaScript</h1>
+    <p>
+      Este é o arquivo app.jsx que será o nosso ponto de partida para
+       o desenvolvimento de aplicação react utilizando JavaScript.
+    </p>
+
+     <BoasVindas nome="Gabriel" />
+     <BoasVindas nome="Andre" />
+     <BoasVindas nome="Breno" />
+     <BoasVindas nome="Rodrigo" />
+     <BoasVindas nome="João Pedro" />
+
+   </div> 
+  )
+}
+
+export default App
