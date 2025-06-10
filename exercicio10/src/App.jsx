214365d60f3efda393ed17481a47ef3f6743525c
Gabrielhenriquemelo <gabrielhenriquemelo116@gmail.com>
2025-06-10 12:13:16 -0300

    10/06/25

---
+++ exercicio10/src/App.jsx
@@ -0,0 +1,11 @@
+import Exercicio10 from "./components/Exercicio10";
+
+export default function app ()
+{
+
+  return(
+    <div>
+      <Exercicio10 dis={1000} vel={500}/>
+      </div>
+  )
+}
---
+++ exercicio10/src/components/Exercicio10.jsx
@@ -0,0 +1,12 @@
+export default function Exercicio10 ({dis, vel})
+{
+    let tempo= dis / vel
+
+    return(
+        <div>
+            <p>A distancia é de: {dis} Km</p>
+            <p>A velocidade é: {vel} Km/h</p>
+            <p>O tempo estimado em horas de viagem é {tempo} horas</p>
+        </div>
+    );
+}
