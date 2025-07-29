9fcd29de05d06b89bf31a8d119ba19e55748c7e3
Gabrielhenriquemelo <gabrielhenriquemelo116@gmail.com>
2025-07-29 12:24:38 -0300

    29/07/25

---
+++ estudocss/src/App.jsx
@@ -0,0 +1,35 @@
+import "./app.css";
+
+export default function App()
+{
+    const estilo = {
+        color : "white",
+        textAlign : "center",
+        backgroundColor : "black"
+    };
+    return (
+        <div className="conteudo">
+            <h1>Estudo CSS</h1>
+            <p>
+                O CSS (<i>Castading style sheets </i>) é uma linguagem utilizada para estilizar as <b>paginas web</b>, com ela podemos definir:
+            </p>
+
+            <ul>
+                <li>cores de fundo, do texto e dos elementos.</li>
+                <li>Fontes e tamanho das letras</li>
+                <li>Posicionamento dos elementos na pagina</li>
+                <li>Espaçamento, bordas, sombras, etc.</li>
+                <li>Efeitos visuais e animações</li>
+            </ul>
+
+            <h3 style={estilo}>Escudo do Corinthians</h3>
+
+            <p className="centro">
+
+                <img src="coraçao.jpeg" />
+
+            </p>
+
+        </div>
+    );
+}
